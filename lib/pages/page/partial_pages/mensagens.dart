0e44f45483c6256a926dbc014ff87e8ef97b3d52
Moacir Roger <100392409+Roger-snts@users.noreply.github.com>
2024-08-16 01:10:54 -0300

    Finalizando página de mensagens

--- lib/pages/page/partial_pages/mensagens.dart
+++ lib/pages/page/partial_pages/mensagens.dart
@@ -27,64 +27,222 @@ class Mensagens extends StatelessWidget {
                 )),
           ],
         ),
-        const Row(
-          children: [
-            SizedBox(
-              height: 330,
-              width: 250,
-              child: const Column(
-                children: [
-                  Image(
-                    image: AssetImage("images/bote.png"),
-                    fit: BoxFit.fill,
-                    height: 260,
-                    width: 250,
-                  ),
-                  Spacer(),
-                  Row(
+        const SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: SizedBox(
+                  height: 330,
+                  width: 250,
+                  child: const Column(
                     children: [
-                      Text(
-                        "Espanha",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 16),
+                      Image(
+                        image: AssetImage("images/bote.png"),
+                        fit: BoxFit.fill,
+                        height: 260,
+                        width: 250,
                       ),
                       Spacer(),
-                      Icon(Icons.star),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.pin_drop,
-                        color: Colors.amber,
-                      ),
-                      Text("Madrid, Espanha"),
-                      Spacer(),
-                      Stack(
-                        fit: StackFit.loose,
-                        alignment: AlignmentDirectional.left,
+                      Row(
                         children: [
-                          ImageIcon(
-                            AssetImage("images/icone.png"),
-                            color: Colors.blue,
+                          Text(
+                            "Espanha",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
                           ),
-                          ImageIcon(
-                            AssetImage("images/icone.png"),
-                            color: Colors.purple,
+                          Spacer(),
+                          Icon(Icons.star),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.pin_drop,
+                            color: Colors.amber,
                           ),
-                          ImageIcon(
-                            AssetImage("images/icone.png"),
-                            color: Colors.green,
+                          Text("Madrid, Espanha"),
+                          Spacer(),
+                          Stack(
+                            fit: StackFit.loose,
+                            alignment: AlignmentDirectional.centerStart,
+                            children: [
+                              ImageIcon(
+                                AssetImage("images/icone.png"),
+                                color: Colors.green,
+                              ),
+                              SizedBox(
+                                width: 40,
+                                child: Positioned(
+                                  left: 80,
+                                  child: ImageIcon(
+                                    AssetImage("images/icone.png"),
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 60,
+                                child: Positioned(
+                                  left: 20,
+                                  child: ImageIcon(
+                                    AssetImage("images/icone.png"),
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ),
+                            ],
                           )
                         ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: SizedBox(
+                  height: 330,
+                  width: 250,
+                  child: const Column(
+                    children: [
+                      Image(
+                        image: AssetImage("images/barquinho.png"),
+                        fit: BoxFit.fill,
+                        height: 260,
+                        width: 250,
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Text(
+                            "França",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Spacer(),
+                          Icon(Icons.star),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.pin_drop,
+                            color: Colors.amber,
+                          ),
+                          Text("París, França"),
+                          Spacer(),
+                          Stack(
+                            fit: StackFit.loose,
+                            alignment: AlignmentDirectional.centerStart,
+                            children: [
+                              ImageIcon(
+                                AssetImage("images/icone.png"),
+                                color: Colors.green,
+                              ),
+                              SizedBox(
+                                width: 40,
+                                child: Positioned(
+                                  left: 80,
+                                  child: ImageIcon(
+                                    AssetImage("images/icone.png"),
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 60,
+                                child: Positioned(
+                                  left: 20,
+                                  child: ImageIcon(
+                                    AssetImage("images/icone.png"),
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: SizedBox(
+                  height: 330,
+                  width: 250,
+                  child: const Column(
+                    children: [
+                      Image(
+                        image: AssetImage("images/montanhas.png"), 
+                        fit: BoxFit.fill,
+                        height: 260,
+                        width: 250,
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Text(
+                            "Barcelona",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16),
+                          ),
+                          Spacer(),
+                          Icon(Icons.star),
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.pin_drop,
+                            color: Colors.amber,
+                          ),
+                          Text("Madrid, Barcelona"),
+                          Spacer(),
+                          Stack(
+                            fit: StackFit.loose,
+                            alignment: AlignmentDirectional.centerStart,
+                            children: [
+                              ImageIcon(
+                                AssetImage("images/icone.png"),
+                                color: Colors.green,
+                              ),
+                              SizedBox(
+                                width: 40,
+                                child: Positioned(
+                                  left: 80,
+                                  child: ImageIcon(
+                                    AssetImage("images/icone.png"),
+                                    color: Colors.blue,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 60,
+                                child: Positioned(
+                                  left: 20,
+                                  child: ImageIcon(
+                                    AssetImage("images/icone.png"),
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         )
       ],
     ));
